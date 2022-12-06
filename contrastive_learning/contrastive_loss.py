99e704a03318e089197fb4b4617d35fe1b091080
Ziqin Luo <850804396@qq.com>
2022-12-06 21:04:01 +0800

    simcse_loss添加device参数

--- contrastive_learning/contrastive_loss.py
+++ contrastive_learning/contrastive_loss.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 #* 输入 embeds 为 形如 [2*batch_size, dim] 的 embeddings
 #* 其中正样本相邻排列, i.e. (0,1), (2,3), (4,5), ... 互为正样本
 #* output 为 平均之后的 对比损失
-def simcse_loss(embeds, tau):
+def simcse_loss(embeds, tau, device):
     N, dim = embeds.shape
     idxs = torch.arange(0, N)
     idxs_1 = idxs[None,:]   # 等价于 idxs_1 = idxs.expand((1,-1)), 将 shape 从 [B]=>[1,B]
@@ -13,7 +13,7 @@ def simcse_loss(embeds, tau):
     labels = (idxs_1.expand((N,N))==idxs_2.expand((N,N))).float()
     embeds = F.normalize(embeds, p=2 ,dim=1)    # L2-normalization
     similarities = torch.matmul(embeds,embeds.T)
-    similarities = similarities - torch.eye(N)*1e12
+    similarities = similarities - torch.eye(N).to(device)*1e12
     similarities = similarities/tau
     loss = F.cross_entropy(input=similarities, target=labels)
     return loss
